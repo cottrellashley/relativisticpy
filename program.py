cd5197a7d9ee5a2f843bf1adda05882d3fae29ca
Ashley Cottrell <ashleydecottrell@gmail.com>
2023-02-14 22:38:21 +0000

    First commit

--- program.py
+++
@@ -1,5 +0,0 @@
-from src.relativisticpy.tensor.gr_tensor import GrTensor
-
-tensor = GrTensor("[[-A(r),0,0,0],[0,B(r),0,0],[0,0,r**2,0],[0,0,0,r**2*smp.sin(theta)**2]]", "_{a}_{b}", "[t, r, theta, phi]")
-
-print(tensor)
