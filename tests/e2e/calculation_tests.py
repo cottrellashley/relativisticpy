23278a1bd6fb7cb9ea76019c8f09864f3e1e7b63
Ashley Cottrell <ashleydecottrell@gmail.com>
2023-02-15 00:07:42 +0000

    e2e files added

---
+++ tests/e2e/calculation_tests.py
@@ -0,0 +1,3 @@
+# Tests are meant to only check whether the package is returning the correct answers.
+# Have lots of use cases here and examples in multiple dimentions.
+# Test for many different known errors and also for wrong asnwers.
