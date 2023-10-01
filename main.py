8127b8de850b67ea925427567ae97474ae95e76b
Ashley Cottrell <ashleydecottrell@gmail.com>
2023-10-01 17:56:55 +0100

    added pip config

--- main.py
+++ main.py
@@ -9,8 +9,22 @@ if __name__ == '__main__':
 
     # print(wb.exe('/Users/ashleycottrell/code/repositories/relativisticpy/.example-files/black_hole.txt'))
 
-    print(wb.exe('.example-files/basic_calculus_example.txt'))
+    # print(wb.exe('.example-files/basic_calculus_example.txt'))
 
     # print(wb.exe('/Users/ashleycottrell/code/repositories/relativisticpy/.example-files/Schild_solution.txt'))
 
-    # print(wb.expr('numerical(pi + e, 20) ** 5.8598744820488384738 '))
+    print(wb.expr("""
+    MetricSymbol := G 
+    RicciSymbol := Ric 
+    Coordinates := [t, r, theta, phi] 
+    G_{mu}_{nu} := [[-A(r),0,0,0], [0,B(r),0,0], [0,0,r**2,0], [0,0,0,r**2*sin(theta)**2]] 
+    eq0 = Ric_{mu:0}_{nu:0} 
+    eq1 = Ric_{mu:1}_{nu:1} 
+    eq2 = Ric_{mu:2}_{nu:2} 
+    eq5 = (eq0*B(r) + eq1*A(r))*(r*B(r)) 
+    solutionB = dsolve(eq5, B(r)) 
+    eq6 = simplify(subs(eq2, B(r), C1/A(r))) 
+    A = dsolve(eq6, A(r)) 
+    B = 1/A 
+    [[-A,0,0,0], [0,B,0,0], [0,0,r**2,0], [0,0,0,r**2*sin(theta)**2]] 
+    """))
